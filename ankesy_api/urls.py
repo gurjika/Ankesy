e123c68deeb01d1ab7dd3525acb9038e7478c6bf
Luka <lgurjidze@gmail.com>
2024-11-29 17:12:57 +0400

    add list vieew for reports

--- ankesy_api/urls.py
+++ ankesy_api/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 
 router.register(prefix='reports', viewset=views.ReportViewSet, basename='report')
+router.register(prefix='list-reports', viewset=views.ListReportViewSet, basename='list-report')
 
 urlpatterns = [
     path('scam-types/', view=views.ScamTypeView.as_view(), name='scam-types')
